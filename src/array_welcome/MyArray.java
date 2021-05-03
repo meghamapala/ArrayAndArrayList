d977701e82604f147afb72012b0c8c11962b9fe6
Mapala1965 <mapalamegha@gmail.com>
2021-05-03 01:53:48 -0400

    first commit

---
+++ src/array_welcome/MyArray.java
@@ -0,0 +1,19 @@
+package array_welcome;
+
+public class MyArray {
+
+	public static void main(String[] args) {
+		
+		
+		int a []= {2,3,4,5,6,7,8};
+		
+		System.out.println(a.length);
+		
+		for (int i=0; i<7; i++) {
+		
+		System.out.println(a[i]);
+		}
+
+	}
+
+}
